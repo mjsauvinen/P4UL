Bugfix in beta function calculation

pyLib/mapTools.py
@@ -466,7 +466,7 @@ def canopyBetaFunction(nind,height,dpz,alpha,beta,lai,d_integral):
   lad_d=np.zeros(nind)
   for z in xrange(nind):
     zHrel= z*dpz/height # z/h
-    lad_d[z]=float((zHrel**(alpha-1)*(1-zHrel)**(beta-1)))/float(d_integral[0])
+    lad_d[z]=float((zHrel**(alpha-1)*(1-zHrel)**(beta-1)))/float(d_integral)
   lad=np.multiply(lad_d,lai)
   return lad
 

pyRaster/findChildDomainOffset.py
@@ -15,12 +15,9 @@ parts and requires special care when editing the file.
 '''
 
 #==========================================================#
-parser = argparse.ArgumentParser(prog='findChildDomainOffset.py',
-                                 description='''Calculates child domain's offset to parent domain in local and global coordinates.''')
-parser.add_argument("-fc", "--child", metavar="CHILD",
-                    type=str, help="Child domain raster data file (.npz).")
-parser.add_argument("-fp", "--parent", metavar="PARENT",
-                    type=str, help="Parent domain raster data file (.npz).")
+parser = argparse.ArgumentParser(prog='findChildDomainOffset.py', description='''Calculates child domain's offset to parent domain in local and global coordinates.''')
+parser.add_argument("-fc", "--child", metavar="CHILD", type=str, help="Child domain raster data file (.npz).")
+parser.add_argument("-fp", "--parent", metavar="PARENT", type=str, help="Parent domain raster data file (.npz).")
 args = parser.parse_args()
 writeLog(parser, args)
 
@@ -48,12 +45,10 @@ RdictChild = None
 print(' Global origo: [N,E] = [{}, {}]'.format(*ROrigChild))
 print(' Size: [N,E] = [{}, {}]'.format(*nPxChild))
 print(' Resolution: [dPy,dPx] = [{}, {}]'.format(*dPxChild))
-print(' Grid rotation: [deg] = {}'.format(
-    gridRotChild / (np.pi / 180.))); print('')
+print(' Grid rotation: [deg] = {}'.format(gridRotChild / (np.pi / 180.))); print('')
 
 if (gridRot != gridRotChild):
   sys.exit('Rotations of parent and child domain don\'t match! Exiting...')
-
 
 # Calculate bottom left origos
 ROrigParentBL = ROrigParent.copy()
@@ -68,8 +63,7 @@ OrigOffset = ROrigChildBL - ROrigParentBL
 print(' Bottom left origo offsets:')
 OrigOffsetLocal = OrigOffset / dPxParent
 print(' Parent domain\'s grid: [N,E] = [{}, {}]'.format(*OrigOffset))
-print(' Pixels in parent domain\'s grid: [N,E]= [{}, {}]'.format(
-    *OrigOffsetLocal))
+print(' Pixels in parent domain\'s grid: [N,E]= [{}, {}]'.format(*OrigOffsetLocal))
 
 # Help the user to move the child domain to match the parent's grid
 if (not(OrigOffsetLocal[0].is_integer() and not(OrigOffsetLocal[1].is_integer()))):

pyRaster/rasterToCanopy3D.py
@@ -15,7 +15,7 @@ Writes PLANT_CANOPY_DATA_3D for PALM from input raster data.
 parser = argparse.ArgumentParser(prog='rasterToCanopy3D.py', description='''Writes PLANT_CANOPY_DATA_3D for PALM from input raster data.''')
 parser.add_argument("rfile", type=str, nargs='?', default=None, help="Name of the input raster data file.")
 parser.add_argument("-fo", "--fileout", type=str, help="Name of the output 3D data file.")
-parser.add_argument("-dz", "--dZ", type=float, help="Resolution of z axis. Defaults to resolution of N axis.")
+parser.add_argument("-dz", "--dZ", type=float, help="Resolution of the z axis. Defaults to resolution of N axis.")
 parser.add_argument("-a", "--alpha", type=float, help="Dimensionless coefficient required for constructing the leaf area density (LAD) profile, using beta probability density function (Markkanen et al., 2003, BLM 106, 437-459).")
 parser.add_argument("-b", "--beta", type=float, help="Dimensionless coefficient required for constructing the leaf area density (LAD) profile, using beta probability density function.")
 parser.add_argument("-l", "--lai", type=float, help="Leaf area index LAI value. LAI is the vertical integral over the LAD profile.")
@@ -45,7 +45,7 @@ print(" 3D grid array dimensions [x,y,z]: {}, {}, {}".format(*np.shape(canopy)))
 print(" Calculating vertical profiles of leaf area densities...")
 
 # Calclulate the value of the integral in the denominator only once
-d_integral = (gamma(args.alpha)*gamma(args.beta))/gamma(alpha+beta)
+d_integral = (gamma(args.alpha)*gamma(args.beta))/gamma(args.alpha+args.beta)
 
 # Calculate leaf area density profiles for each horizontal grid tile and fill array vertically
 for x in xrange(nPx3D[1]):
@@ -59,7 +59,7 @@ for x in xrange(nPx3D[1]):
 print(" ...done.")
 
 # Write output data file
-print(" Writing output output file...")
+print(" Writing output file...")
 fx = open(args.fileout, 'w')
 # The first row of the file is number of vertical canopy layers
 fx.write(str(nPx3D[2])+"\n")