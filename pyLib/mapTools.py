Bugfix in canopy profile calculation

pyLib/mapTools.py
@@ -460,15 +460,15 @@ def farFieldMean( dat, Xcoord, excludeNearest ):
 
 # =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
-def canopyBetaFunction(nind,height,dpz,alpha,beta,lai,d_integral):
+def canopyBetaFunction(height,dpz,alpha,beta,lai):
   '''
   Calculate lead area index using beta probability density function
   (Markkanen et al., 2003, BLM 106, 437-459).
   '''
-  lad_d=np.zeros(nind)
-  for z in xrange(nind):
-    zHrel= z*dpz[2]/height # z/h
-    lad_d[z]=float((zHrel**(alpha-1)*(1-zHrel)**(beta-1)))/float(d_integral)
+  from scipy.stats import beta as betadist
+  z_col=np.arange(0.,height/dpz[2],dpz[2])
+  z_col=np.divide(z_col,height) # z/H
+  lad_d = betadist.pdf(z_col,alpha,beta)/height
   lad=np.multiply(lad_d,lai)
   return lad
 

pyLib/netcdfTools.py
@@ -273,7 +273,7 @@ def createCoordinateAxis(dso, Rdims, Rdpx, axis, varname, formatstr, unit, param
 
 def fillTopographyArray(Rtopo, Rdims, Rdpx, datatype):
   topodims = np.array([Rdims[2], Rdims[0], Rdims[1]])
-  topo = np.ones(topodims, dtype=datatype)
+  topo = np.zeros(topodims, dtype=datatype)
   print(' \n Filling 3D array from topography data...')
   print(' Dimensions [z,y,x]: [{}, {}, {}]'.format(*topodims))
   print(' Total number of data points: {}'.format(np.prod(topodims)))
@@ -281,7 +281,7 @@ def fillTopographyArray(Rtopo, Rdims, Rdpx, datatype):
     for y in xrange(Rdims[0]):
       # Reverse the y-axis because of the top-left origo in raster
       maxind = int(round(Rtopo[-y - 1][x] / Rdpx[2]))
-      topo[0:maxind, y, x] = 0
+      topo[0:maxind, y, x] = 1
   print(' ...done. \n')
   return topo
 

pyRaster/rasterToCanopy3D.py
@@ -4,7 +4,6 @@ import argparse
 import numpy as np
 from utilities import writeLog
 from mapTools import *
-from scipy.special import gamma
 
 '''
 Description:
@@ -44,9 +43,6 @@ canopy = np.zeros([nPx3D[1], nPx3D[0], nPx3D[2]])
 print(" 3D grid array dimensions [x,y,z]: {}, {}, {}".format(*np.shape(canopy)))
 print(" Calculating vertical profiles of leaf area densities...")
 
-# Calclulate the value of the integral in the denominator only once
-d_integral = (gamma(args.alpha)*gamma(args.beta))/gamma(args.alpha+args.beta)
-
 # Calculate leaf area density profiles for each horizontal grid tile and fill array vertically
 for y in xrange(nPx3D[0]):
   for x in xrange(nPx3D[1]):
@@ -57,8 +53,8 @@ for y in xrange(nPx3D[0]):
       continue
     nind = int(np.floor(canopyHeight / float(dPx3D[2])))+1 # Number of layers
     # Calculate LAD profile
-    lad = canopyBetaFunction(nind,canopyHeight,dPx3D,args.alpha,args.beta,args.lai,d_integral)
-    canopy[x,y,0:nind] = lad
+    lad = canopyBetaFunction(canopyHeight,dPx3D,args.alpha,args.beta,args.lai)
+    canopy[x,y,0:nind-1] = lad # Grid point at canopy top level gets value 0
 print(" ...done.")
 
 # Write output data file
@@ -69,6 +65,9 @@ fx.write(str(nPx3D[2])+"\n")
 # Loop through the verical canopy columns
 for x in xrange(nPx3D[1]):
   for y in xrange(nPx3D[0]):
+    if (np.all(canopy[x,y,:]==0)):
+      # There is no need to write empty columns
+      continue
     # Convert everything into strings and write
     # datatype x y col(:)
     lineStr = str(1)+","+str(x)+","+str(y)+","+",".join(map(str,canopy[x,y,:]))+"\n"