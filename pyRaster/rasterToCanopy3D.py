58c1527046a2962b0c9b27f9ae8abd4b669ffb9e
Karttunen Sasu M <sasu.karttunen@helsinki.fi>
2017-12-28 14:39:27 +0200

    rasterToCanopy3D.py: added a possibility to output a 3D array mask instead of a text file. This is helpful when implementing morphometric methods (under work).

--- pyRaster/rasterToCanopy3D.py
+++ pyRaster/rasterToCanopy3D.py
@@ -4,6 +4,11 @@ import argparse
 import numpy as np
 from utilities import writeLog
 from mapTools import *
+from netcdfTools import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
 
 '''
 Description:
@@ -19,6 +24,8 @@ parser.add_argument("-dz", "--dZ", type=float, help="Resolution of the z axis. D
 parser.add_argument("-a", "--alpha", type=float, help="Dimensionless coefficient required for constructing the leaf area density (LAD) profile, using beta probability density function (Markkanen et al., 2003, BLM 106, 437-459).")
 parser.add_argument("-b", "--beta", type=float, help="Dimensionless coefficient required for constructing the leaf area density (LAD) profile, using beta probability density function.")
 parser.add_argument("-l", "--lai", type=float, help="Leaf area index LAI value. LAI is the vertical integral over the LAD profile.")
+parser.add_argument("-am", "--asmask", type=str, default=None, help="Output a 3D array mask instead of a text file formatted for PALM. The argument is a file type, use nc for NetCDF4 output and npz for Numpy Z array.")
+parser.add_argument("-t", "--threshold", type=float, default=0.0, help="Threshold LAD value to be used when generating a 3D mask. Grid points with a LAD value over the threshold will be set to 1 while the rest is set to 0. Effective only if --asmaks is set.")
 args = parser.parse_args()
 writeLog(parser, args)
 
@@ -35,10 +42,9 @@ Rdict = readNumpyZTile( filename )
 R = Rdict['R']
 nPx = np.shape(R)
 dPx = Rdict['dPx']
-Rdict = None
 
 # Calculate the shape of the new 3D array, use largest in-canopy value
-nPx3D = nPx; RdPx3D = dPx
+nPx3D = nPx; dPx3D = dPx
 if ( dZ ):
   dPx3D = np.append(dPx, args.dZ)
 else:
@@ -48,7 +54,8 @@ nPx3D = np.append(nPx3D, int(np.floor(np.amax(R)/dPx3D[2])+1))
 
 # Fill the 3D canopy array
 canopy = np.zeros([nPx3D[1], nPx3D[0], nPx3D[2]])
-print(" 3D grid array dimensions [x,y,z]: {}, {}, {}".format(*np.shape(canopy)))
+nPx3D=[nPx3D[1], nPx3D[0], nPx3D[2]]
+print(" 3D grid array dimensions [x,y,z]: {}, {}, {}".format(*nPx3D))
 print(" Calculating vertical profiles of leaf area densities...")
 
 # Calculate leaf area density profiles for each horizontal grid tile and fill array vertically
@@ -70,18 +77,41 @@ print(" ...done.")
 
 # Write output data file
 print(" Writing output file...")
-fx = open( fileout, 'w')
-# The first row of the file is number of vertical canopy layers
-fx.write(str(nPx3D[2])+"\n")
-# Loop through the verical canopy columns
-for x in xrange(nPx3D[1]):
-  for y in xrange(nPx3D[0]):
-    if (np.all(canopy[x,y,:]==0)):
-      # There is no need to write empty columns
-      continue
-    # Convert everything into strings and write
-    # datatype x y col(:)
-    lineStr = str(1)+","+str(x)+","+str(y)+","+",".join(map("{:.3g}".format,canopy[x,y,:]))+"\n"
-    fx.write(lineStr)
-fx.close()
+if (args.asmask):
+  # Use threshold value to create a mask raster.
+  canopymask=np.zeros(np.shape(canopy))
+  canopymask[np.where(canopy>args.threshold)]=1
+  if (args.asmask=="npz"):
+    # Save as Numpy Z file.
+    Rdict["R"]=canopymask
+    saveTileAsNumpyZ( fileout, Rdict )
+  elif (args.asmask=="nc"):
+    # Save as netCDF4
+    # Maybe someday PALM can read canopy data from NetCDF4
+    dso = netcdfOutputDataset(args.fileout)
+    # Create dimensions
+    xv = createCoordinateAxis(dso, nPx3D, dPx3D, 1, 'x', 'f4', 'm', parameter=True)
+    yv = createCoordinateAxis(dso, nPx3D, dPx3D, 0, 'y', 'f4', 'm', parameter=True)
+    zv = createCoordinateAxis(dso, nPx3D, dPx3D, 2, 'z', 'f4', 'm', parameter=True)
+    # Due to a bug Paraview cannot read x,y,z correctly so rolling to z,y,x
+    canopymask=np.rollaxis(canopymask,2)
+    canopymask=np.swapaxes(canopymask,1,2)
+    masknc = createNetcdfVariable(dso, canopymask, "canopy_0", 0, 'm', 'i4', ('z', 'y', 'x'), parameter=False)
+    netcdfWriteAndClose(dso)
+
+else:
+  fx = open( fileout, 'w')
+  # The first row of the file is number of vertical canopy layers
+  fx.write(str(nPx3D[2])+"\n")
+  # Loop through the verical canopy columns
+  for x in xrange(nPx3D[1]):
+    for y in xrange(nPx3D[0]):
+      if (np.all(canopy[x,y,:]==0)):
+        # There is no need to write empty columns
+        continue
+      # Convert everything into strings and write
+      # datatype x y col(:)
+      lineStr = str(1)+","+str(x)+","+str(y)+","+",".join(map("{:.3g}".format,canopy[x,y,:]))+"\n"
+      fx.write(lineStr)
+  fx.close()
 print(" ...{} saved successfully.".format(fileout))
