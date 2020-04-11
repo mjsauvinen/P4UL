af92f59be8a0a869cff360f756f90963c4fbc9e3
Mikko J.S. Auvinen <mikko.auvinen@gmail.com>
2020-04-11 22:07:51 +0300

    Small improvement in pyLib/plotTools.py (3)

--- pyLib/plotTools.py
+++ pyLib/plotTools.py
@@ -300,7 +300,7 @@ def addToPlot(fig, x,y,labelStr, plotStr=["","",""], logOn=False):
       lines=ax.semilogy(x,yt,linestyle_stack(), linewidth=1.3, label=labelStr)
       #lines=ax.loglog(x,yt,'-', linewidth=1.3, label=labelStr)
     else:
-      lines=ax.plot(x,yt,linestyle_stack(lm=linemode), linewidth=1.6, label=labelStr)
+      lines=ax.plot(x,yt,linestyle_stack(), linewidth=1.6, label=labelStr)
   ax.set_title( plotStr[0], fontsize=22)
   ax.set_xlabel(plotStr[1], fontsize=22)
   ax.set_ylabel(plotStr[2], fontsize=22); ax.grid(True)  
