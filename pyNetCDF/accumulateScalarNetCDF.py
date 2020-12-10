8d285a8649fdc395f5e39274e784e893a774a2c0
Mikko J.S. Auvinen <mikko.auvinen@fmi.fi>
2020-12-10 16:32:56 +0200

    Improved pyNetCDF/accumulateScalarNetCDF.py.

--- pyNetCDF/accumulateScalarNetCDF.py
+++ pyNetCDF/accumulateScalarNetCDF.py
@@ -26,6 +26,25 @@ def readCoords_nc(ds, tskip, sStr):
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
 
+def write1dProfile( st, zt, fn, maskOn=False):
+    saz = np.mean( st[-1,:,:,:], axis=(1,2))  # compute all, and correct afterwards
+    
+    if( maskOn ):
+      nz, ny, nx = np.shape( st[-1,:,:,:] )
+      ids = np.ones( (ny,nx) , bool )
+      ids[388:410,228:250] = False  # 1) UniqAir
+      ids[197:218,248:271] = False  # 2) UniqAir
+      for k in range(1,60):
+        saz[k] = np.mean( st[-1,k,ids] )
+      ids = None
+
+
+    #print(' saz.shape = {}, saz = {} '.format(np.shape(saz), saz))
+    hStr = ' z-coord (m), < s >_xy '
+    np.savetxt( fn , np.c_[ zt.ravel() , saz.ravel() ], fmt='%3.6e', header=hStr)
+    saz = None
+    
+
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
 
 #==========================================================#
@@ -40,6 +59,8 @@ parser.add_argument("-pc", "--percentile", type=int, default=None,\
   help="Store given percentile instead. Overrides the standard functionality.")
 parser.add_argument("-nt", "--ntimeskip", type=int, help="Skip <nt> number of time steps.",\
   default=0)
+parser.add_argument("-tx", "--timeExtension",type=float, nargs=2, default=[None,None],\
+  help="Extend time to [timeX] with given increment [dtX] in seconds. Example usage: -tx timeX dtX ")
 parser.add_argument("-zp", "--zprofile", action="store_true", default=False,\
   help="Compute vertical profile of final accumulation.")
 parser.add_argument("-M", "--useMask", action="store_true", default=False,\
@@ -55,7 +76,7 @@ cl          = 1
 zprofile    = args.zprofile
 useMask     = args.useMask
 pc          = args.percentile
-
+timex, dtx  = args.timeExtension
 
 parameter = True;  variable  = False
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = #
@@ -85,18 +106,12 @@ for fn in fileNos:
   if( pc is None ): fs = 'ACC_'
   else:             fs = 'P{:02d}_'.format(pc)
   
-  dso = netcdfOutputDataset(fs+fileList[fn].split('/')[-1], mode="w")
-  tv = createNetcdfVariable( dso, time,'time', ntimes,'s','f4',('time',), parameter )
-  xv = createNetcdfVariable( dso, x, 'x', len(x), 'm', 'f4', ('x',), parameter )
-  yv = createNetcdfVariable( dso, y, 'y', len(y), 'm', 'f4', ('y',), parameter )
-  zv = createNetcdfVariable( dso, z, 'z', len(z), 'm', 'f4', ('z',), parameter )
-
-
-  sa = np.zeros( np.shape(s) )
+  # = = = = = = = = = = = = = = = = = = = = = = #
+  sa = np.zeros( s_dims )
   
   if( pc is None ):
     # Work here 
-    sx = 'a'
+    sffx = 'a'
     #rStot = 1./np.sum( s[1,:,:,:] )
     rStot = 1.  #/np.percentile( s[1,:,:,:], 99 )
   
@@ -107,7 +122,7 @@ for fn in fileNos:
     
     
   else: # percentile
-    sx = 'p'
+    sffx = 'p'
     for i in range(1, ntimes ):
       idl = (s[i,:,:,:] > 1.E-5)
       Px  = np.percentile( s[i,idl], pc )
@@ -115,29 +130,56 @@ for fn in fileNos:
       
       idp = (s[i,:,:,:] >= Px)
       sa[i,idp] = s[i,idp]
+      
+  s1 = np.mean( s[-8:,:,:,:], axis=0 )   
+  s  = None # Clear memory
+  
+  # = = = = = = = = = = = = = = = = = = = = = = #
+
+  if( timex is not None  and dtx is not None ):
+    ntimesx = (timex-time[-1])/dtx
+    ntimesx = np.round( ntimesx, decimals=0).astype(int)  # Convert to int
+    tx = np.linspace(time[-1], timex, ntimesx)
     
-  s = None # Clear memory
+    sx_dims = np.array(s_dims); sx_dims[0] = ntimesx
+    sx = np.zeros( sx_dims )
+    sx[0,:,:,:] = sa[-1,:,:,:]*rStot
+    for i in range(1,ntimesx):
+      sx[i,:,:,:] = sx[i-1,:,:,:] + s1[:,:,:]*dtx*rStot
+    
+    s1 = None # Clear memory
+    
+    dsx = netcdfOutputDataset('ACCX_'+fileList[fn].split('/')[-1], mode="w")
+    tvx = createNetcdfVariable( dsx, tx,'time', ntimesx,'s','f4',('time',), parameter )
+    xvx = createNetcdfVariable( dsx, x, 'x', len(x), 'm', 'f4', ('x',), parameter )
+    yvx = createNetcdfVariable( dsx, y, 'y', len(y), 'm', 'f4', ('y',), parameter )
+    zvx = createNetcdfVariable( dsx, z, 'z', len(z), 'm', 'f4', ('z',), parameter )
+    svx = createNetcdfVariable(\
+      dsx, sx, scalarName+'x', sx_dims[0], '', 'f4',('time','z','y','x',), variable)
+    netcdfWriteAndClose( dsx )
+    
+    if( zprofile ):
+      write1dProfile( sx, z,'sx_horiz-mean_'+fileList[fn].split('/')[-1] , useMask ) 
+    
+    sx = None # Clear memory
+    svx = zvx = yvx = xvx = tvx = dsx = None
+  
+  # = = = = = = = = = = = = = = = = = = = = = = #
   
   if( zprofile ):
-    saz = np.mean( sa[-1,:,:,:], axis=(1,2))  # compute all, and correct afterwards
-    
-    if( useMask ):
-      nz, ny, nx = np.shape( sa[-1,:,:,:] )
-      ids = np.ones( (ny,nx) , bool )
-      ids[388:410,228:250] = False  # 1) UniqAir
-      ids[197:218,248:271] = False  # 2) UniqAir
-      for k in range(1,60):
-        saz[k] = np.mean( sa[-1,k,ids] )
-      ids = None
-
-    print(' saz.shape = {}, saz = {} '.format(np.shape(saz), saz))
-    hStr = ' z-coord (m), < sa >_xy '
-    np.savetxt('horiz-mean_'+fileList[fn].split('/')[-1], np.c_[ z.ravel() , saz.ravel() ], fmt='%3.6e', header=hStr)
+    write1dProfile( sa, z,'sa_horiz-mean_'+fileList[fn].split('/')[-1] , useMask )    
   
-
+  # = = = = = = = = = = = = = = = = = = = = = = #
+  
+  dso = netcdfOutputDataset(fs+fileList[fn].split('/')[-1], mode="w")
+  tv = createNetcdfVariable( dso, time,'time', ntimes,'s','f4',('time',), parameter )
+  xv = createNetcdfVariable( dso, x, 'x', len(x), 'm', 'f4', ('x',), parameter )
+  yv = createNetcdfVariable( dso, y, 'y', len(y), 'm', 'f4', ('y',), parameter )
+  zv = createNetcdfVariable( dso, z, 'z', len(z), 'm', 'f4', ('z',), parameter )
   sv = createNetcdfVariable(\
-    dso, sa, scalarName+sx, s_dims[0], '', 'f4',('time','z','y','x',), variable)
+    dso, sa, scalarName+sffx, s_dims[0], '', 'f4',('time','z','y','x',), variable)
   
+  sa = None
   #hStr = '# time, {0}th percentile({1}), mean({1}), std({1}) '.format(percentile,sname)
   #np.savetxt(sname+'.dat', np.c_[ time, sp, sm, ss ], fmt='%3.6e', header=hStr)
 
