efccf5fb66ea9cf94cec1a68c10184473ad6ad6a
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2020-12-21 17:27:43 +0200

    Added the gaussian filter to filterRaster.

--- pyRaster/filterRaster.py
+++ pyRaster/filterRaster.py
@@ -19,10 +19,13 @@ parser.add_argument("-fo", "--fileout",type=str,
                     help="Name of output raster file.")
 parser.add_argument("-n","--size", type=int, nargs=2, default=[5,5],
                     help="Size (x,y) of filtering window. Default=5×5.")
+parser.add_argument("-s","--sigma", type=float, default=None, help="Standard" 
+     " deviation (σ). Invokes the"+"\033[1m"+" Gaussian filter"+"\033[0m"+".")
 args = parser.parse_args()
 filename= args.filename
 fileout = args.fileout
 fsize = np.array( args.size)
+sigma = args.sigma
 #=input=======================================================================#
 Rdict = readNumpyZTile(filename)
 R = Rdict['R']
@@ -31,7 +34,12 @@ ROrig = Rdict['GlobOrig']
 print(' Rdims = {} '.format(Rdims))
 print(' ROrig = {} '.format(ROrig))
 #=filtering===================================================================#
-R=sn.median_filter(R,size=fsize)
+if (sigma is not None):
+  print('Applying the gaussian filter.')
+  R=sn.gaussian_filter(R,sigma)
+else:
+  print('Applying the median filter.')
+  R=sn.median_filter(R,size=fsize)
 #=output======================================================================#
 Rdict['R'] = np.round( R , decimals=1 )
 saveTileAsNumpyZ( fileout, Rdict )
