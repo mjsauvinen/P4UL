37e4bf8933004e48003dbe11334d16a7646274e8
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2021-01-04 15:15:08 +0200

    Added minimum filter to pyFilter.

--- pyRaster/filterRaster.py
+++ pyRaster/filterRaster.py
@@ -9,11 +9,11 @@ from mapTools import readNumpyZTile, saveTileAsNumpyZ
 Jukka-Pekka Keskinen
 jukka-pekka.keskinen@fmi.fi
 Finnish Meteorological Insitute
-2020
+2020-2021
 '''
 #=arg parser==================================================================#
 parser = argparse.ArgumentParser(prog='filterRaster.py',
-                                 description='Filter an npz raster.')
+    description='Filter a P4UL raster. Only one filter is applied at a time.')
 parser.add_argument("-f", "--filename",type=str, 
                     help="Name of the input raster data file.")
 parser.add_argument("-fo", "--fileout",type=str, 
@@ -21,11 +21,14 @@ parser.add_argument("-fo", "--fileout",type=str,
 parser.add_argument("-n","--size", type=int, nargs=2, default=[5,5],
                     help="Size (x,y) of filtering window. Default=5×5.")
 parser.add_argument("-s","--sigma", type=float, default=None, help="Standard" 
-     " deviation (σ). Invokes the"+"\033[1m"+" Gaussian filter"+"\033[0m"+".")
-parser.add_argument("-t","--maximum",action="store_true", default=False, help="Invoke "
-" the "+"\033[0m"+"maximum filter"+"\033[0m"+".")
-parser.add_argument("-m","--median",action="store_true", default=False, help="Invoke "
-" the "+"\033[0m"+"median filter"+"\033[0m"+".")
+     " deviation (σ). Invokes the"+"\033[1m"+" Gaussian filter"+"\033[0m"+"."
+                    " Does not use filter size even if specified.")
+parser.add_argument("-t","--maximum",action="store_true", default=False, 
+    help="Invoke the "+"\033[0m"+"maximum filter"+"\033[0m"+".")
+parser.add_argument("-m","--median",action="store_true", default=False, 
+    help="Invoke the "+"\033[0m"+"median filter"+"\033[0m"+".")
+parser.add_argument("-b","--minimum",action="store_true", default=False, 
+    help="Invoke the "+"\033[0m"+"minimum filter"+"\033[0m"+".")
 args = parser.parse_args()
 filename= args.filename
 fileout = args.fileout
@@ -33,6 +36,7 @@ fsize = np.array( args.size)
 sigma = args.sigma
 maxi  = args.maximum
 medi  = args.median
+mini  = args.minimum 
 #=input=======================================================================#
 Rdict = readNumpyZTile(filename)
 R = Rdict['R']
@@ -42,16 +46,19 @@ print(' Rdims = {} '.format(Rdims))
 print(' ROrig = {} '.format(ROrig))
 #=filtering===================================================================#
 if (sigma is not None):
-  print('Applying the gaussian filter.')
+  print(' Applying the gaussian filter.')
   R=sn.gaussian_filter(R,sigma)
 elif maxi:
-  print('Applying the maximum filter.')
+  print(' Applying the maximum filter.')
   R=sn.maximum_filter(R,size=fsize)
 elif medi:
-  print('Applying the median filter.')
+  print(' Applying the median filter.')
   R=sn.median_filter(R,size=fsize)
+elif mini:
+  print(' Applying the minumum filter.')
+  R=sn.minimum_filter(R,size=fsize)
 else:
-  print('No filter specified. Nothing to do here.')
+  print(' No filter specified. Nothing to do here.')
   sys.exit()
 #=output======================================================================#
 Rdict['R'] = np.round( R , decimals=1 )
