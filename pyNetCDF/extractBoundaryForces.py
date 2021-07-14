848edf7f64607c912549f6504e2f9dbc3dc19238
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2021-07-14 15:56:58 +0300

    Added netCDF output for drag time series in extractBoundaryForces.py.

--- pyNetCDF/extractBoundaryForces.py
+++ pyNetCDF/extractBoundaryForces.py
@@ -17,12 +17,15 @@ parser.add_argument('-f', '--filenames',type=str, nargs='+', help=
 parser.add_argument('-A', '--cellArea',type=float, default=1.0,
                     help='Cell area for pressure force calculations. '
                     'Default: 1m².')
+parser.add_argument('-o', '--outputFile',type=str, default=None, help=
+                    'Save output forces to the given netCDF file.')
 args = parser.parse_args()
 
 #==========================================================#
 # Initial renaming operations and variable declarations
 
 filenames = args.filenames
+outputfile = args.outputFile
 area = args.cellArea
 #==========================================================#
 
@@ -33,13 +36,19 @@ if 'p' in varList:
   print('  Pressure found from variables, calculating drag.')
   print('  Using cell area '+str(area)+' in pressure force calculation \n')
   aikakoko = ds['p'].shape[0]
-  ds0 = None
+  aika = ds['time'][:].data
+  ds.close()
+  ds = None
   varList = None
   paramList = None
 
+  Dx=-9999.0*np.ones(aikakoko)
+  Dy=-9999.0*np.ones(aikakoko)
+
+  print('  * * * Drag * * *')
   for ti in range(aikakoko):
     psumx = 0.0
-    psumy = 0.0
+    psumy = 0.0    
     for tiedosto in filenames:
       ds, varList, paramList = netcdfDataset(tiedosto,False)
       itr = ds['p'].shape
@@ -64,10 +73,22 @@ if 'p' in varList:
             raki = np.where(pm)[0][-1]+1
             if raki < np.size(p):
               psumy = psumy - p[raki]*area
-    print('  * * * Drag * * *')
-    print('  time:  '+np.array2string(ds['time'][ti],precision=0)+' s')
-    print('  X:     '+np.array2string(psumx, precision=1)+' N')
-    print('  Y:     '+np.array2string(psumy, precision=1)+' N')
-    print('  total: '+np.array2string(np.sqrt(psumx**2+psumy**2), precision=1)+' N')
+      ds.close()      
+    Dx[ti] = psumx
+    Dy[ti] = psumy
+    print('  time: '+np.array2string(aika[ti],precision=0)+' s')
+    print('     X:     '+np.array2string(psumx, precision=0)+' N')
+    print('     Y:     '+np.array2string(psumy, precision=0)+' N')
+    print('     total: '+np.array2string(np.sqrt(psumx**2+psumy**2), precision=0)+' N')
+  if not outputfile==None:
+    dso = netcdfOutputDataset(outputfile)
+    print('\n Outputting drag data to file '+outputfile)
+    createNetcdfVariable(dso, aika, 'time', aikakoko, 'seconds', 'f8', ('time',), True)
+    createNetcdfVariable(dso, Dx, 'Drag x', aikakoko, 'Newtons', 'f8', ('time',), False,
+                         fill_value=-9999.0)
+    createNetcdfVariable(dso, Dy, 'Drag y', aikakoko, 'Newtons', 'f8', ('time',), False,
+                         fill_value=-9999.0)
+    dso.close()
+
 else:
   print('  Pressure not found from variables, nothing to do.')
