4b4ab97f3bf7225608c723f67d1b5665111a972f
Mikko J.S. Auvinen <mikko.auvinen@fmi.fi>
2022-01-11 09:31:52 +0200

    Updated pyRaster/tif2NumpyTile.py which will now accept ascii txt format as well.

--- pyRaster/tif2NumpyTile.py
+++ pyRaster/tif2NumpyTile.py
@@ -17,7 +17,8 @@ Author: Mikko Auvinen
 parser = argparse.ArgumentParser(prog='tif2NumpyTile.py')
 parser.add_argument("-f", "--filename",type=str, help="Input tif-image file name.")
 parser.add_argument("-fo", "--fileout",type=str, help="Output npz file name.")
-parser.add_argument("-r", "--reso",type=float, help="Resolution of the tif-image.")
+parser.add_argument("-r", "--reso",type=float, nargs=2, \
+                    help="Resolution of the tif-image [Dx Dy].")
 parser.add_argument("-xo", "--xorig",type=float, nargs=2,default=[0.,0.],\
   help="Coords [N,E] of the tif-images top-left corner. Default=[0,0]")
 parser.add_argument("-p", "--printOn", help="Print the numpy array data.",\
@@ -26,6 +27,8 @@ parser.add_argument("-pp", "--printOnly", help="Only print the numpy array data.
   action="store_true", default=False)
 parser.add_argument("-s", "--scale",type=float, default=1.,\
    help="Scale factor for the output. Default=1.")
+parser.add_argument("-a", "--ascii", action="store_true", default=False, \
+   help="Input is an ASCII-formatted raster with no metadata (such as TOPOGRAPHY_DATA). Default=False.")
 args = parser.parse_args()
 writeLog( parser, args, args.printOnly )
 #==========================================================#
@@ -38,10 +41,14 @@ ROrig     = args.xorig
 printOn   = args.printOn
 printOnly = args.printOnly
 sc        = args.scale
+ascii     = args.ascii
 
+if(ascii):
+  R = np.loadtxt(filename)
+else:
+  R = openTifAsNumpy(filename)
 
-R = openTifAsNumpy(filename)
-dPx      = np.array([sc*reso, sc*reso])
+dPx      = np.array([sc*reso[0], sc*reso[1]])
 Rdict = {'R' : R, 'GlobOrig' : ROrig, 'gridRot' : 0., 'dPx' : dPx}
 
 if( not printOnly ):
