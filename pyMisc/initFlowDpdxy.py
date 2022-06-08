2c49c2dd8a63e7c1fa2c1a8853274db2132183bd
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2022-06-08 14:47:29 +0300

    Possibility to indicate the file where the initial velocity profile is read from.

--- pyMisc/initFlowDpdxy.py
+++ pyMisc/initFlowDpdxy.py
@@ -5,10 +5,13 @@ import argparse
 
 #==========================================================#
 parser = argparse.ArgumentParser(prog='flow_init_dpdxy.py')
-parser.add_argument("-wd", "--winddir", type=float, default=270.,\
-  help="Wind direction in degrees relative to grid: 0 equals north (-y), 180 south (+y), 270 west (+x). Default = 270.")
+parser.add_argument("-wd", "--winddir", type=float, default=270.,
+  help="Wind direction in degrees relative to grid: 0 equals north (-y),"
+                    " 180 south (+y), 270 west (+x). Default = 270.")
 parser.add_argument("-s", "--scale", type=float, default=1.,\
   help="Scaling factor [s] for the wind values: Uout = s*Uin.")
+parser.add_argument("-f", "--filename", type=str, default="umag_profil.dat",
+                    help="File with reference wind profile.")
 args = parser.parse_args()
 #==========================================================#
 
@@ -17,7 +20,7 @@ alpha  = (270. - args.winddir ) * np.pi/180.
 
 # = = = = = = = = = = = = = = = 
 
-z, Umag = np.loadtxt('umag_profil.dat', usecols=(0,1), unpack=True)
+z, Umag = np.loadtxt(args.filename, usecols=(0,1), unpack=True)
 dpmag   = 0.0018  # Turku setup 
 
 u = scale * Umag * np.cos( alpha )
