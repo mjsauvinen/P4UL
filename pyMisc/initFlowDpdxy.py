ac743a1ce013ba821887537b1e2553f3862d71d8
Jukka-Pekka Keskinen <jukka-pekka.keskinen@fmi.fi>
2022-06-20 10:45:58 +0300

    Better scaling of input wind profiles for initFlowDpdxy.py.

--- pyMisc/initFlowDpdxy.py
+++ pyMisc/initFlowDpdxy.py
@@ -8,8 +8,12 @@ parser = argparse.ArgumentParser(prog='flow_init_dpdxy.py')
 parser.add_argument("-wd", "--winddir", type=float, default=270.,
   help="Wind direction in degrees relative to grid: 0 equals north (-y),"
                     " 180 south (+y), 270 west (+x). Default = 270.")
-parser.add_argument("-s", "--scale", type=float, default=1.,\
-  help="Scaling factor [s] for the wind values: Uout = s*Uin.")
+parser.add_argument("-U", "--Umax", type=float, default=None,\
+  help="Maximum value for wind magnitude in the profile. Used for scaling "
+"the magnitude of the wind profile.")
+parser.add_argument("-z", "--zmax", type=float, default=None,\
+  help="Maximum height of the wind profile. Used for scaling the extent "
+"of the wind profile..")
 parser.add_argument("-f", "--filename", type=str, default="umag_profil.dat",
                     help="File with reference wind profile.")
 parser.add_argument("-dp", "--pressuregrad", type=float, default=0.002,
@@ -17,16 +21,31 @@ parser.add_argument("-dp", "--pressuregrad", type=float, default=0.002,
 args = parser.parse_args()
 #==========================================================#
 
-scale  = args.scale 
-alpha  = (270. - args.winddir ) * np.pi/180.
-
+alpha   = (270. - args.winddir ) * np.pi/180.
 # = = = = = = = = = = = = = = = 
 
 z, Umag = np.loadtxt(args.filename, usecols=(0,1), unpack=True)
+maxUmag = np.max(Umag)
+maxz    = np.max(z)
+
+if args.Umax==None:
+    Uscale = maxUmag
+else:
+    Uscale  = args.Umax
+if args.zmax==None:
+    zscale = maxz
+else:
+    zscale  = args.zmax 
+
+# Normalise profiles. If already normalised, nothing happens
+# If no scaling is specified, use input profiles directly.
+z = z/maxz
+Umag = Umag/maxUmag
+
 dpmag   = args.pressuregrad
 
-u = scale * Umag * np.cos( alpha )
-v = scale * Umag * np.sin( alpha )
+u = Uscale * Umag * np.cos( alpha )
+v = Uscale * Umag * np.sin( alpha )
 
 pdx = -dpmag * np.cos( alpha )
 pdy = -dpmag * np.sin( alpha )
@@ -34,7 +53,7 @@ pdy = -dpmag * np.sin( alpha )
 
 ustr = ' '.join('{:.1f},'.format(ui) for ui in u[::6])
 vstr = ' '.join('{:.1f},'.format(vi) for vi in v[::6])
-zstr = ' '.join('{:.1f},'.format(zi) for zi in z[::6])
+zstr = ' '.join('{:.1f},'.format(zscale*zi) for zi in z[::6])
 
 u_output = 'u_profile  = {}'.format(ustr)
 v_output = 'v_profile  = {}'.format(vstr)
