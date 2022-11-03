ec6672add49d8143243798b3f8778333858109ee
Mikko J.S. Auvinen <mikko.auvinen@fmi.fi>
2022-11-03 09:48:26 +0200

    Removed comment for testing.

--- pyNetCDF/vectorDecompNetCdf.py
+++ pyNetCDF/vectorDecompNetCdf.py
@@ -118,7 +118,7 @@ args = parser.parse_args()
 writeLog( parser, args )
 
 #==========================================================#
-# Initial renaming operations and variable declarations. That's all.
+# Initial renaming operations and variable declarations. 
 
 filename   = args.filename
 fileout    = args.fileout
